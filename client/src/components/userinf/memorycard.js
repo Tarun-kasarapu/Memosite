3239cd9b58f0940fcc029ef349f20b44194bd731
Tarun-kasarapu <ktarunkasaraput@gmail.com>
2023-05-27 19:11:22 +0530

    second committ

---
+++ client/src/components/userinf/memorycard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Memorycard(props  ){
+
+    return (
+        <div>
+            <h1>HEllo</h1>
+            <img alt='not' src={logo}/>
+        </div>
+    )
+}
+export default Memorycard;
